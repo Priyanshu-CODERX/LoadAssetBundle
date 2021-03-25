889be7cfd2c9946fa7d74a982333ffd828c7d99b
Priyanshu Bhattacharjee <pbo4oh@gmail.com>
2021-03-25 20:55:45 +0530

    added files

---
+++ Assets/Scripts/LoadAssetBundle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+public class LoadAssetBundle : MonoBehaviour
+{
+   	public string bundleURL = "";
+   	public string assetName = "";
+
+   	IEnumerator Start()
+   	{
+        string url = bundleURL;
+        var request
+            = UnityEngine.Networking.UnityWebRequestAssetBundle.GetAssetBundle(url, 0);
+        yield return request.SendWebRequest();
+        AssetBundle bundle = UnityEngine.Networking.DownloadHandlerAssetBundle.GetContent(request);
+        GameObject cube = bundle.LoadAsset<GameObject>(assetName);
+        Instantiate(cube);
+   	}
+}
